b620d39ee256e039b743e74b263c14896a605f6f
2yeseul <20152917@sungshin.ac.kr>
2021-01-19 22:33:38 +0900

    no message

--- src/main/java/study/spring/project/HelloController/HelloController.java
+++ src/main/java/study/spring/project/HelloController/HelloController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // 테스트
-// ㅇㅇ
+// ?
+
 @Controller
 public class HelloController {
 
